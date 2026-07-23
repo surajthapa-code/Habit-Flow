a3901b0f3726a78f68bf7d5c3f83d9b3287d49cc
kashish706 <kashishsrivastava276@gmail.com>
2026-07-23 13:22:09 +0530

    fix: remove unused useNavigate import

--- frontend/src/pages/ProgressCalendarPage.js
+++ frontend/src/pages/ProgressCalendarPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { getHabits } from '../api';
 import MonthlyCalendar from '../components/MonthlyCalendar';
 
